1378fa618a7813203598ded829f99db54e9ac5a7
Trevor Esparza <trevoresparza30@gmail.com>
2022-05-03 22:36:28 -0500

    end of part 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -212,6 +212,34 @@ console.log('boo')
 
 console.log(kristin.getSummary());
 
+//// CODING CHALLENGE 3 /////
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBmi: function (){
+        this.bmi = this.mass/this.height ** 2;
+        return this.bmi
+    }
+}
+
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBmi: function (){
+        this.bmi = this.mass/this.height ** 2;
+        return this.bmi
+    }
+}
+
+mark.calcBmi();
+john.calcBmi();
+console.log(john.bmi,mark.bmi)
 
 
-
+if(mark.bmi > john.bmi){
+    console.log(`${mark.fullName}'s BMI of ${mark.bmi} is higher than ${john.fullName}'s BMI of ${john.bmi}`)
+}else{
+    console.log(`${john.fullName}'s BMI of ${john.bmi}is higher thaN ${mark.fullName}'s BMI of ${mark.bmi}`)
+}
